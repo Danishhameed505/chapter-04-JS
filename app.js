6de6547cec56dd98fcd601c80a1f9160a60f88ed
Danishhameed505 <155366241+Danishhameed505@users.noreply.github.com>
2024-03-08 19:25:28 +0500

    chapter-04-JS
    chapter-04-JS

---
+++ app.js
@@ -0,0 +1,32 @@
+// Q # 01
+// Ans:
+var variable1 = "Hello, World!";
+console.log(variable1);
+var variable2 = "Testing...";
+console.log(variable2);
+var variable3 = 42;
+console.log(variable3);
+
+// Q # 02
+// Ans:
+// illegal variable
+
+// 01 A space is an illegal variable like (name 1)
+// 02 var _$; illegal variables
+// 03 var 1danish= its an illegal variable
+// 04 reserve key words are illegal variable
+// 05 we are not start &,%,#,@ these are illegal variables
+
+// legal variable
+// 01 var danish1= legal variable
+// 02 test type snake n camle is legal variable
+// 03 we can start with $ sign its legal variablel
+// 04 both uppercase and lowercase), numbers, underscores, and dollar signs 
+
+// Q # 03: Display this in your browser
+// Ans:
+document.write("<h1>Rules for naming JS variables</h1> <br>")
+document.write("Variable names can only contain,numbers,$ and_.for example: $my_1stVariable <br>")
+document.write("Variables must begin with a letter, $ or _ . for example: $name, _ name or name <br>")
+document.write("Variable names are case sensitive <br>")
+document.write("Variable names should not be JS Keywords <br>")
